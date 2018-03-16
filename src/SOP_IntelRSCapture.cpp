d93346299bf1fc865bb9278d60e4bb2ac1f20a65
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2018-03-16 23:35:12 +0100

    Working color and uvs.

--- src/SOP_IntelRSCapture.cpp
+++ src/SOP_IntelRSCapture.cpp
@@ -68,27 +68,13 @@ SOP_RSCapture::cookMySop(OP_Context &context)
     OP_AutoLockInputs inputs(this);
     if (inputs.lock(context) >= UT_ERROR_ABORT)
         return error();
-    gdp->clearAndDestroy();
-
-
-    // Flag the SOP as being time dependent (i.e. cook on time changes)
-    // rs2::pointcloud pc;
-    // We want the points object to be persistent so we can display the last cloud when a frame drops
-    // rs2::points points;
-
-    // Declare RealSense pipeline, encapsulating the actual device and sensors
-    // rs2::pipeline pipe;
-    // pipe.start();
- 
     
 
     rs2::frameset frames = pipe.wait_for_frames();
     const bool result = (frames.size() != 0);
-    // const bool result = pipe.poll_for_frames(&frames);
 
     if (!result) {
         addWarning(SOP_MESSAGE, "No capture.");
-        // pipe.stop();
         return error();
     }
 
@@ -99,47 +85,49 @@ SOP_RSCapture::cookMySop(OP_Context &context)
         return error();
     }
     // Generate the pointcloud and texture mappings
-    // points.reset();
-    // rs2::pointcloud pc;
     points = pc.calculate(depth);
+    const size_t nverts = points.size();
+    if (nverts == 0) {
+        addWarning(SOP_MESSAGE, "No points.");
+        return error();
+    }
+    
     rs2::video_frame color = frames.get_color_frame();
     if (!color) {
         addWarning(SOP_MESSAGE, "No depth frame");
-        // pipe.stop();
         return error();
     }
     pc.map_to(color);
+
     const int height = color.get_height();
     const int width  = color.get_width();
     const uint8_t * rgb_buff = static_cast<const uint8_t*>(color.get_data());
 
     const rs2::vertex * vertices        = points.get_vertices();
     const rs2::texture_coordinate * uvs = points.get_texture_coordinates();
-    const size_t nverts = points.size();
 
-    if (nverts == 0)
-    {
-        addWarning(SOP_MESSAGE, "No points.");
-        // pipe.stop();
-        return error();
-    }
-    GA_Offset ptoff = gdp->appendPointBlock(nverts);
+    gdp->clearAndDestroy();
+    gdp->appendPointBlock(nverts); 
 
-    GA_RWHandleV3 colorh(gdp->addDiffuseAttribute(GA_ATTRIB_POINT));
+    GA_RWHandleV3 cdh(gdp->addDiffuseAttribute(GA_ATTRIB_POINT));
+    GA_RWHandleV3 uvh(gdp->addFloatTuple(GA_ATTRIB_POINT, "uv", 3));
 
+    GA_Offset ptoff;
     GA_FOR_ALL_PTOFF(gdp, ptoff) {
         const GA_Index index  = gdp->pointIndex(ptoff);
         const rs2::vertex & v = vertices[index];
         const rs2::texture_coordinate & uv = uvs[index];
-        const int x = SYSfloor(uv.u * width);
-        const int y = SYSfloor(uv.v * height);
-        const uint8_t r       = rgb_buff[y*width+x+0];
-        const uint8_t g       = rgb_buff[y*width+x+1];
-        const uint8_t b       = rgb_buff[y*width+x+2];
-        const UT_Vector3 cd(r*1.0f/255.0f, g*1.0f/255.0f, b*1.0f/255.0f);
+        const int   x = SYSfloor(uv.u * width)*3;
+        const int   y = SYSfloor(uv.v * height)*3;
+        const float r = SYSpow((float)rgb_buff[y*width+x+0]/255.0f, 2.2f);
+        const float g = SYSpow((float)rgb_buff[y*width+x+1]/255.0f, 2.2f);
+        const float b = SYSpow((float)rgb_buff[y*width+x+2]/255.0f, 2.2f);
+        const UT_Vector3 cdv(r, g, b);
+        const UT_Vector3 uvv(uv.u, 1.f - uv.v, 0);
         const UT_Vector3 pos(v.x, v.y, v.z);
         gdp->setPos3(ptoff, pos);
-        colorh.set(ptoff, cd);
+        cdh.set(ptoff, cdv);
+        uvh.set(ptoff, uvv);
     }
 
     gdp->getP()->bumpDataId();
